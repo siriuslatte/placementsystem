ab377272e5740ef0c0fe1ebd081fe8e36df8b1a1
siriuslatte <zeydiscorddev.business@gmail.com>
2022-01-14 11:29:50 -0600

    feat: Initial commit and set-up for project

---
+++ out/client/main.client.lua
@@ -0,0 +1,4 @@
+-- Compiled with roblox-ts v1.2.9
+local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
+local makeHello = TS.import(script, game:GetService("ReplicatedStorage"), "TS", "module").makeHello
+print(makeHello("main.client.ts"))
